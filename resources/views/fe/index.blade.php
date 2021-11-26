ca31142eccb8f55932076a0bf013f32c5451f013
FebriTahta <febrirtah@gmail.com>
2021-11-26 14:17:18 +0700

    deskripsi

--- resources/views/fe/index.blade.php
+++ resources/views/fe/index.blade.php
@@ -64,7 +64,7 @@
 					<a href="{{$item->link}}" target="_blank" style="text-decoration: none" class="accordion-column col-sm-6">
 						<!-- Accordian Box -->
 						<ul class="accordion-box" style="text-align: center">	
-							<div class="accordion block">
+							<div class="card">
 								<div class="acc-btn" style="text-transform: capitalize; text-align: center"> {{$item->name}}</div>
 							</div>
 						</ul>
