82bcb0eedb3128aaad39bdc38edec8586f67bd84
FebriTahta <febrirtah@gmail.com>
2021-11-29 09:25:06 +0700

    login form

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -26,7 +26,7 @@
 	<link rel="stylesheet" href="assets/css/header-colors.css" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
 	@yield('head')
-	<title>LinksTree Lite</title>
+	<title>Media NF</title>
 	<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
 	<link rel="stylesheet" href="{{asset('assets/font-awesome-4.7.0/css/font-awesome.min.css')}}">
 </head>
