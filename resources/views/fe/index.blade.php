6efed6d27b5b228be4b56bf701c076e0a9cccee2
FebriTahta <febrirtah@gmail.com>
2021-11-29 11:13:05 +0700

    fa

--- resources/views/fe/index.blade.php
+++ resources/views/fe/index.blade.php
@@ -26,6 +26,19 @@
 	 content="{{ asset('be_img_aplikasi_thumb/1637900067.jpeg') }}"
 	 @endif 
 	>
+	<style>
+		.fab .fa-tiktok {
+        color: #111111;
+        filter: drop-shadow(-5px -5px 0 #24f6f0) contrast(150%) brightness(110%);
+        z-index: -1;
+        }
+
+        .fab .fa-tiktok::after {
+        filter: drop-shadow(5px 5px 0 #F70250) contrast(150%) brightness(110%);
+        z-index: -1;
+        }
+	</style>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
 @endsection
 
 @section('content')
