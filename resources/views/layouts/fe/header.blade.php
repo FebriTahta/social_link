7ad485d284aa2f77c49636e6827dfe9baf19dca5
FebriTahta <febrirtah@gmail.com>
2021-11-29 12:14:27 +0700

    fa

--- resources/views/layouts/fe/header.blade.php
+++ resources/views/layouts/fe/header.blade.php
@@ -12,7 +12,7 @@
      dalam diklat tilawati. download e-certificate anda disini"/>
 <meta property="og:image" itemprop="image" content="{{ asset('assets/images/tumb.jpeg') }}">
 @yield('head')
-
+<link rel="stylesheet" href="{{asset('assets/font-awesome-4.7.0/css/font-awesome.min.css')}}">
 {{-- <link href="https://fonts.googleapis.com/css2?family=Niconne&family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Tangerine:wght@400;700&display=swap" rel="stylesheet"> --}}
 
 <link rel="shortcut icon" href="{{ asset('corak/nf.png') }}" type="image/x-icon">
