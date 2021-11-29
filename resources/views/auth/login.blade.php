26179ab51b30deffb6118dd25f439e1cd6a29f8f
FebriTahta <febrirtah@gmail.com>
2021-11-29 09:23:12 +0700

    login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -70,4 +70,64 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection --}}
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Media NF</title>
+    <link rel="stylesheet" type="text/css" href="{{asset('login_template/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('login_template/css/fontawesome-all.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('login_template/css/iofrm-style.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('login_template/css/iofrm-theme21.css')}}">
+</head>
+<body>
+    <div class="form-body without-side">
+        <div class="website-logo">
+            <a href="index.html">
+                {{-- <div class="logo">
+                    <img class="logo-size" src="{{asset('login_template/images/nf_logo.png')}}" alt="">
+                </div> --}}
+                
+            </a>
+        </div>
+        <div class="row">
+            <div class="img-holder">
+                <div class="bg"></div>
+                <div class="info-holder">
+                    <img src="{{asset('login_template/images/graphic3.svg')}}" alt="">
+                </div>
+            </div>
+            <div class="form-holder">
+                <div class="form-content">
+                    <div class="form-items">
+                        <h3>Login to account</h3>
+                        <p>Kumpulan Media Social dan Aplikasi.</p>
+                        <form method="POST" action="{{ route('login') }}">@csrf
+                            <input class="form-control" type="text" name="name" placeholder="E-mail Address" required>
+                            <input class="form-control" type="password" name="password" placeholder="Password" required>
+                            <div class="form-button">
+                                <button id="submit" type="submit" class="ibtn">Login</button> 
+                                {{-- <a href="forget21.html">Forget password?</a> --}}
+                            </div>
+                        </form>
+                        <div class="other-links">
+                            <div class="text">Belum punya akun ? </div>
+                            {{-- <a href="#"><i class="fab fa-facebook-f"></i>Facebook</a><a href="#"><i class="fab fa-google"></i>Google</a><a href="#"><i class="fab fa-linkedin-in"></i>Linkedin</a> --}}
+                        </div>
+                        <div class="page-links">
+                            <a class="btn btn-info text-white" href="#">Daftar</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<script src="{{asset('login_template/js/jquery.min.js')}}"></script>
+<script src="{{asset('login_template/js/popper.min.js')}}"></script>
+<script src="{{asset('login_template/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('login_template/js/main.js')}}"></script>
+</body>
+</html>
