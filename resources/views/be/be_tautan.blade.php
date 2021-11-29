d9d3b8bf508e79d270287ec60b3665f3c385d12c
FebriTahta <febrirtah@gmail.com>
2021-11-29 11:16:46 +0700

    fa

--- resources/views/be/be_tautan.blade.php
+++ resources/views/be/be_tautan.blade.php
@@ -20,6 +20,7 @@
         }
     </style>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="{{asset('assets/font-awesome-4.7.0/css/font-awesome.min.css')}}">
 @endsection
 
 @section('content')
